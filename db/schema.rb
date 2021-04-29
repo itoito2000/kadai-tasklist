e420109834c46eebbfef856ef4390624a7074f8f
itoi.shimizu <itoito200048310@gmail.com>
2021-04-29 07:42:33 +0000

    reset db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_29_012852) do
+ActiveRecord::Schema.define(version: 2021_04_29_004407) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
